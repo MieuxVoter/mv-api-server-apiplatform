f7210c997ad2506997e48b39196d2506ba58edc8
domi41 <dominique.merle.41@gmail.com>
2020-10-04 16:59:26 +0200

    docs

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,15 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
+/**
+ * This application needs no fixtures for the moment.
+ * This file allows us to leverage the database reset utilities of fixtures in the gherkin suite.
+ *
+ * We might add some preset mentions.  Might not.  Clients ought to do it?
+ *
+ * Class AppFixtures
+ * @package App\DataFixtures
+ */
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
