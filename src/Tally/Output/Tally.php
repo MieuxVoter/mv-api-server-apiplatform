c8eb85fccbaadb69004a1f702c670d98881e43ea
domi41 <dominique.merle.41@gmail.com>
2020-10-05 21:01:05 +0200

    chore: flag dusty things

--- src/Tally/Output/Tally.php
+++ src/Tally/Output/Tally.php
@@ -5,6 +5,8 @@ namespace App\Tally\Output;
 
 
 /**
+ * NOT USED
+ *
  * A count of the votes.
  * The value are floats instead of integers because we may have complex tallybots with transitivity friction|entropy.
  * The purpose of a TallyBot is to generate and output an instance of this.
