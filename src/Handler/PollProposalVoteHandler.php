9e2c3b3f5305f4a0fbf3d710533904a6780056df
domi41 <dominique.merle.41@gmail.com>
2020-10-05 12:28:02 +0200

    feat: add a handler for votes

---
+++ src/Handler/PollProposalVoteHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Handler;
+
+
+use App\Entity\Poll;
+use App\Entity\PollProposal;
+use App\Entity\PollProposalVote;
+use App\Entity\User;
+
+
+/**
+ * Exists to patch https://github.com/api-platform/docs/issues/504
+ * Subresources should handle POST.  Since they don't yet, we do this "by hand".
+ *
+ * Class PollProposalVoteHandler
+ * @package App\Handler
+ */
+class PollProposalVoteHandler
+{
+
+    public function handleVote(PollProposalVote $vote, User $judge, PollProposal $proposal, Poll $poll)
+    {
+//        $vote->setProposal($proposal);
+        // Instead we use the inverse, it sets both
+        $proposal->addVote($vote);
+        $vote->setElector($judge);
+        return $vote;
+    }
+
+}
