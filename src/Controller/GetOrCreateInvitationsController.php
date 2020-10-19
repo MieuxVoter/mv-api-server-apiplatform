0a839d6a52279aa0dbbe733c34159956ff2db3c4
domi41 <dominique.merle.41@gmail.com>
2020-10-19 10:33:26 +0200

    feat: Implement invitation generation on-demand.
    I know we talked about "synthetic" invitations.
    
    - less secure than random
    - we need an entity anyway
    - benchmark → limits, contact us to bump them
    - postgres should handle the 100000 invites we need for majorichons.fr

--- src/Controller/GetOrCreateInvitationsController.php
+++ src/Controller/GetOrCreateInvitationsController.php
@@ -45,19 +45,59 @@ class GetOrCreateInvitationsController
 
     /**
      * @param Request $request
+     * @return array
      */
     public function __invoke(Request $request): array
     {
+        $pollId = $request->get("pollId");
         /** @var PollInvitationRepository $invitationsRepo */
         $invitationsRepo = $this->em->getRepository(Invitation::class);
-        $pollId = $request->get("pollId");
         /** @var Poll $poll */
         $poll = $this->em->getRepository(Poll::class)->findOneByUuid($pollId);
 
-        // FIXME:
-        // Read existing Invitations
-        // Generate missing Invitations
+        $defaultLimit = 10; // app config?
+        $limit = $request->get("limit", $defaultLimit);
+        $defaultOffset = 0;
+        $offset = 0; // todo
 
-        return $this->em->getRepository(Invitation::class)->findAll();
+        $invitations = [];
+
+        // Read existing Invitations
+        // We could order them by creation date instead of numerical id
+        $existingInvitations = $invitationsRepo->findBy(['poll'=>$poll], ['id' => 'ASC'], $limit, $offset);
+        $invitations = $invitations + $existingInvitations;
+
+        $missingInvitationsAmount = $limit - count($invitations);
+        if ($missingInvitationsAmount > 0) {
+            // Generate missing Invitations
+            //
+            // Database Concern(s)
+            // -------------------
+            // Let's limit the amount of invitations one can generate.
+            // Admins may ignore that limit.
+            // We might do things like allowing more invitations
+            // once a quota of existing invitations has been "consumed".
+            // Or allow overriding the quota from a user property or role.
+            // Game design, etc.
+            // Anyways, this need benchmarking.
+            //
+            // Security Concern(s)
+            // -------------------
+            // We're generating invitations in fast sequence,
+            // and perhaps UUIDv4 pseudo-random could be reproduced to guess invitations.
+            // UUIDv4 relies on random_bytes, if I'm reading this right.
+            // https://www.php.net/manual/en/function.random-bytes.php
+            // … "cryptographically secure". Perhaps not enough for us (sequence!).
+            for ($i = 0; $i < $missingInvitationsAmount; $i++) {
+                $invitation = new Invitation();
+                $invitation->setPoll($poll);
+                $this->em->persist($invitation);
+                $invitations[] = $invitation;
+            }
+
+            $this->em->flush();
+        }
+
+        return $invitations;
     }
 }
