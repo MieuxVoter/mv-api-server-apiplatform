b59ecbb59b0f1bccbb93e4ad8e32c4b7036550cb
domi41 <dominique.merle.41@gmail.com>
2020-10-31 19:03:29 +0100

    fix: remove /api prefix

--- src/Swagger/JwtDecorator.php
+++ src/Swagger/JwtDecorator.php
@@ -51,7 +51,7 @@ final class JwtDecorator implements NormalizerInterface
 
         $tokenDocumentation = [
             'paths' => [
-                '/api/_jwt' => [
+                '/_jwt' => [
                     'post' => [
                         'tags' => ['Token'],
                         'operationId' => 'postCredentialsItem',
