200570cfafe233f41658caeacca2a031b9cae1a3
domi41 <dominique.merle.41@gmail.com>
2020-11-07 10:50:01 +0100

    chore: clean up

--- src/Repository/PollRepository.php
+++ src/Repository/PollRepository.php
@@ -6,10 +6,12 @@ use App\Entity\Poll;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 
+
 /**
  * @method Poll|null find($id, $lockMode = null, $lockVersion = null)
  * @method Poll|null findOneBy(array $criteria, array $orderBy = null)
  * @method Poll|null findOneByUuid($pollId)
+ * @method Poll|null findOneBySlug($pollSlug)
  * @method Poll[]    findAll()
  * @method Poll[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
@@ -50,6 +52,7 @@ class PollRepository extends ServiceEntityRepository
         return null;
     }
 
+
     protected function findOneWithUuidStartingWith($uuidPrefix)
     {
         $polls = $this->createQueryBuilder('p')
@@ -66,33 +69,4 @@ class PollRepository extends ServiceEntityRepository
         return null;
     }
 
-
-    // /**
-    //  * @return Poll[] Returns an array of Poll objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('l')
-            ->andWhere('l.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('l.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Poll
-    {
-        return $this->createQueryBuilder('l')
-            ->andWhere('l.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
