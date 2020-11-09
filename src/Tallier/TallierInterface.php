0113dc776c32f1e2e2482d483ca9582df7051f78
domi41 <dominique.merle.41@gmail.com>
2020-11-09 22:56:30 +0100

    docs: review before the big-ass refacto of the tallier

--- src/Tallier/TallierInterface.php
+++ src/Tallier/TallierInterface.php
@@ -9,13 +9,13 @@ use App\Tallier\Output\PollTally;
 
 
 /**
- * The purpose of a ResultBot is to create a Result from the Votes and Delegations.
+ * The purpose of a Tallier is to create a PollTally from the Ballots (and perhaps Delegations).
  *
  * Since there is a ocean of possibilities when tallying, you may implement your own TallyBot.
  * It will only need to implement this interface (and perhaps be declared as Service with a special tag).
  *
  * For now, please put your implementations in the same directory as this interface,
- * and suffix them with `TallyBot`, like `NaiveTallyBot`
+ * and suffix them with `Tallier`, like `MyBetterTallier`
  */
 interface TallierInterface
 {
