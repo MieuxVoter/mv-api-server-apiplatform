30acd501f48a2a6dc656bcd090ce7939444d96a5
domi41 <dominique.merle.41@gmail.com>
2020-11-13 05:34:42 +0100

    feat: add options for majority judgment ranking
    These are test options, so we cab experiment
    with the documentation generation for options,
    and constraint validation.
    
    But it's legit, the default grade could be an option of the ranking.
    We'll see in the next meeting.

---
+++ src/Ranking/Options/MajorityJudgmentOptions.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Ranking\Options;
+
+
+class MajorityJudgmentOptions
+{
+
+    protected $default_grade_index = 0;
+
+    ///
+    ///
+
+    /**
+     * @return int
+     */
+    public function getDefaultGradeIndex(): int
+    {
+        return $this->default_grade_index;
+    }
+
+    /**
+     * @param int $default_grade_index
+     */
+    public function setDefaultGradeIndex(int $default_grade_index): void
+    {
+        $this->default_grade_index = $default_grade_index;
+    }
+
+}
