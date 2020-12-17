69cdd6753abaf328b65d2fdc3733335159fdfce1
domi41 <dominique.merle.41@gmail.com>
2020-12-17 12:26:49 +0100

    docs

--- lib/mieuxvoter/MajorityJudgment/Tally/PollTallyInterface.php
+++ lib/mieuxvoter/MajorityJudgment/Tally/PollTallyInterface.php
@@ -8,8 +8,11 @@ namespace MieuxVoter\MajorityJudgment\Tally;
  * A Tally holds the amount of Judgments for each Grade, on each Proposal.
  * It also needs to hold the total amount of Participants, in order to account for default grades.
  *
+ * This is the interface of the main input of this library,
+ * that which is given to the MajorityJudgmentResolver in order to derive a Result (a ranking of proposals).
+ *
  * You may implement this interface in your own classes,
- * or use our implementation of it, `PollTally`.
+ * or use one of our convenience implementations of it, such as `ArrayPollTally`.
  *
  * Interface PollTallyInterface
  * @package MieuxVoter\MajorityJudgment\Tally
@@ -26,6 +29,11 @@ interface PollTallyInterface
     public function getParticipantsAmount() : int;
 
     /**
+     * Tallies for each Proposal.
+     * The order matters only in *perfect* equality scenarios.
+     * In these extreme cases, the order of ex-æquo proposals in the Result
+     * will reflect the order of proposals submitted here.
+     *
      * @return ProposalTallyInterface[]
      */
     public function getProposalsTallies() : iterable;
