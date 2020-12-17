a2f77ae5f3fe48bf6c01a5c3b4c46c4922e02d94
domi41 <dominique.merle.41@gmail.com>
2020-12-17 10:56:45 +0100

    feat: add a convenience class for the PollResults

---
+++ lib/mieuxvoter/MajorityJudgment/Result/GenericPollResult.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace MieuxVoter\MajorityJudgment\Result;
+
+
+/**
+ * Generic, instantiable implementation of PollResultInterface.
+ * This is a convenience class.
+ *
+ * Class GenericPollResult
+ * @package MieuxVoter\MajorityJudgment\Result
+ */
+class GenericPollResult implements PollResultInterface
+{
+
+    protected $ranked_proposals = [];
+
+    /**
+     * GenericPollResult constructor.
+     * @param array $ranked_proposals
+     */
+    public function __construct(iterable $ranked_proposals)
+    {
+        $this->ranked_proposals = $ranked_proposals;
+    }
+
+    /**
+     * @return RankedProposal[]
+     */
+    public function getRankedProposals(): iterable
+    {
+        return $this->ranked_proposals;
+    }
+}
