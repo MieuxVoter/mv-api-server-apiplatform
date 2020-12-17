4fb7d8daf8c7b7344d61583d359f3a8c3f3d665a
domi41 <dominique.merle.41@gmail.com>
2020-12-17 07:36:52 +0100

    test: unit test the MajorityJudgmentResolver
    This is going to be an integration test, more likely.
    We need to chat with @roipoussiere

---
+++ lib/mieuxvoter/MajorityJudgment/Test/MajorityJudgmentResolverTest.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace MieuxVoter\MajorityJudgment\Test;
+
+
+use MieuxVoter\MajorityJudgment\Resolver\MajorityJudgmentResolver;
+use MieuxVoter\MajorityJudgment\Resolver\Options\MajorityJudgmentOptions;
+use MieuxVoter\MajorityJudgment\Tally\ArrayPollTally;
+use PHPUnit\Framework\TestCase;
+
+
+class MajorityJudgmentResolverTest extends TestCase
+{
+
+    public function testResolve()
+    {
+        $amountOfJudgments = 21;
+        $tallyPerProposal = [
+            'proposal_a' => [1, 1, 4, 3, 7, 4, 1],
+            'proposal_b' => [0, 2, 4, 3, 7, 4, 1],
+        ];
+        $expectedResults = [
+            [
+                'proposal' => 'proposal_b',
+                'rank' => 1,
+            ],
+            [
+                'proposal' => 'proposal_a',
+                'rank' => 2,
+            ],
+        ];
+
+        $resolver = new MajorityJudgmentResolver();
+        $options = new MajorityJudgmentOptions();
+        $pollTally = new ArrayPollTally(
+            $amountOfJudgments, $tallyPerProposal
+        );
+        $result = $resolver->resolve($pollTally, $options);
+
+        $i = 0;
+        foreach ($result->getRankedProposals() as $rankedProposal) {
+            $this->assertEquals(
+                $expectedResults[$i]['proposal'],
+                $rankedProposal->proposal,
+                "Proposals are sorted adequately"
+            );
+            $this->assertEquals(
+                $expectedResults[$i]['rank'],
+                $rankedProposal->rank,
+                "Proposals are ranked adequately"
+            );
+            $i++;
+        }
+
+    }
+
+}
