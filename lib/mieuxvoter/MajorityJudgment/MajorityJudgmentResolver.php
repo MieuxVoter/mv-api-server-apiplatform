05498c4ccf73ccf6bd744e78cec8e6be7c959465
domi41 <dominique.merle.41@gmail.com>
2020-12-18 14:56:56 +0100

    feat: implement score-based JM resolution
    This needs more test cases, but all our current tests pass.
    I'm late, gotta flee, no time to explain.

--- lib/mieuxvoter/MajorityJudgment/MajorityJudgmentResolver.php
+++ lib/mieuxvoter/MajorityJudgment/MajorityJudgmentResolver.php
@@ -15,13 +15,15 @@ use MieuxVoter\MajorityJudgment\Model\Tally\ProposalTallyInterface;
 /**
  * Score-based MJ resolver.
  *
+ * TODO: add links to relevant papers and perhaps wikipedia page
  * https://scholar.google.fr/scholar?q=majority+judgment
  *
  * Ideally, since this algorithm is in theory parallelizable per proposal,
  * we should support `parallel`.
  * See https://www.php.net/manual/fr/intro.parallel.php
- *
  * This would enable us to get support for huge amounts of proposals.
+ *
+ * Tests: Tests/MajorityJudgmentResolverTest.php
  *
  * Class MajorityJudgmentResolver
  * @package MieuxVoter\MajorityJudgment\Resolver
@@ -56,21 +58,21 @@ class MajorityJudgmentResolver implements ResolverInterface
      */
     public function resolve(PollTallyInterface $pollTally, $options): PollResultInterface
     {
-        $ranked_proposals = [];
+        $rankedProposals = [];
 
         // I. Compute the score of each proposal, skip the rank for now
         foreach ($pollTally->getProposalsTallies() as $proposalsTally) {
-            $unranked_proposal = self::computeUnrankedProposal(
+            $scoredProposal = self::computeUnrankedProposal(
                 $proposalsTally,
                 $pollTally->getParticipantsAmount(),
                 $options
             );
-            $ranked_proposals[] = $unranked_proposal;
+            $rankedProposals[] = $scoredProposal;
         }
 
         // II. Sort the proposals using their score (higher is "better")
         $sortSuccess = usort(
-            $ranked_proposals,
+            $rankedProposals,
             function(RankedProposal $rpa, RankedProposal $rpb)
             {
                 return strcmp($rpb->getScore(), $rpa->getScore());
@@ -80,23 +82,23 @@ class MajorityJudgmentResolver implements ResolverInterface
 
         // III. Compute the rank of each proposal
         $rank = 1;  // human-centric value, so starts at 1 ("best" proposal)
-        $amountOfProposals = count($ranked_proposals);
+        $amountOfProposals = count($rankedProposals);
         for ($i = 0 ; $i < $amountOfProposals ; $i++) {
 
             if ($i == 0) {
-                $ranked_proposals[$i]->setRank($rank);
+                $rankedProposals[$i]->setRank($rank);
             } else {
                 if (
-                    $ranked_proposals[$i]->getScore()
+                    $rankedProposals[$i]->getScore()
                     ==
-                    $ranked_proposals[$i-1]->getScore()
+                    $rankedProposals[$i-1]->getScore()
                 ) {
                     // Wow, we have a *perfect* ex-æquo!
-                    $ranked_proposals[$i]->setRank(
-                        $ranked_proposals[$i-1]->getRank()
+                    $rankedProposals[$i]->setRank(
+                        $rankedProposals[$i-1]->getRank()
                     );
                 } else {
-                    $ranked_proposals[$i]->setRank($rank);
+                    $rankedProposals[$i]->setRank($rank);
                 }
             }
 
@@ -104,10 +106,13 @@ class MajorityJudgmentResolver implements ResolverInterface
         }
 
         // IV. We've got everything we need, time to build the Result
-        $result = new GenericPollResult($ranked_proposals);
+        $result = new GenericPollResult($rankedProposals);
 
         return $result;
     }
+
+    // This could be derived from the data instead of being set arbitrarily like this
+    const GRADES_AMOUNT_MAX_DIGITS = 3; // 10**3 = 1000 grades should be more than enough
 
     /**
      * Computes the score of the provided proposal.
@@ -131,7 +136,7 @@ class MajorityJudgmentResolver implements ResolverInterface
         // I. Collect data and check its sanity
         $gradesTallies = $proposalTally->getGradesTallies();
         $grades = [];  // "worst" to "best"
-        $tallies = [];  // same order as grades
+        $tallies = [];  // same order as grades, is mutated by algorithm
         foreach ($gradesTallies as $gradeTally) {
             assert(
                 $gradeTally->getProposal() == $proposalTally->getProposal(),
@@ -164,29 +169,198 @@ class MajorityJudgmentResolver implements ResolverInterface
         );
         $defaultGrade = $grades[$defaultGradeIndex];
 
-        // III. Fills the blanks with the default Grade
+        // III. Fill the blanks with the default Grade
         // FIXME
 
         // IV. Compute a lexicographical score (higher is "better")
         $score = "";
         for ($i = 0 ; $i < $amountOfGrades ; $i++) {
-            $median_grade_index = self::getMedianGradeIndex($tallies);
+            if (0 < $i) {
+                $score .= '/';
+            }
+
+            $medianGradeIndex = self::getMedianGradeIndex($tallies);
+            $score .= sprintf(
+                "%0".((string) self::GRADES_AMOUNT_MAX_DIGITS)."d",
+                $medianGradeIndex
+            );
+
+            // Collect biggest of the two groups of grades outside of the median.
+            // Group Grade is the index of the grade in the group that is adjacent to the median group.
+            // Group Sign is:
+            // - +1 if the group promotes higher grades (adhesion)
+            // - -1 if the group promotes lower grades (contestation)
+            // - ±0 if there is no spoon (nor group)
+            [$groupSize, $groupSign, $groupGrade] = self::getBiggestGroup(
+                $medianGradeIndex, $tallies
+            );
+
+            $score .= '_';
+            // TODO: derive amount of needed leading zeroes from $participantsAmount
+            // TODO: replace 100000000000 by $participantsAmount
+            $score .= sprintf(
+                "%012d",
+                100000000000 + $groupSign * $groupSize
+            );
+
+            self::regradeJudgments($tallies, $medianGradeIndex, $groupGrade);
         }
 
+        //dump("Score", $proposalTally->getProposal(), $score);
         $unrankedProposal->setScore($score);
-
-//        foreach ($gradesTallies as $gradeTally) {
-//            $gradeTally->getTally();
-//            // FIXME: resume coding here
-//        }
-
 
         return $unrankedProposal;
     }
 
-    static function getMedianGradeIndex($tallies): int
+    /**
+     * Find the index of the median grade from the given array of tallies.
+     *
+     * This method may be optimized:
+     * - pass $total as parameter
+     * - median-finding loop may perhaps be optimized as well
+     *
+     * @param array $tallies
+     *   Indexed array of integers.
+     *   Tally for each Grade, in the 'worst" grade to "best" grade order.
+     *   A Tally here is an amount of Judgments emitted with a specific Grade.
+     *   This looks like the merit profile, in other words.
+     *   Eg: A value of [1, 4, 3] would mean (Reject=1, Passable=4, Good=3)
+     * @param bool $low
+     *   Use the low (default) or high median, when there's an EVEN amount of judgments.
+     * @return int
+     */
+    static function getMedianGradeIndex($tallies, $low=true): int
     {
-        return 0; // FIXME
+        // We could perhaps pass this $total as parameter,
+        // but that would mean we trust that the total is correct, since
+        // when we use this method we assume that the all the tallies
+        // are already filled with the default values.
+        // Since for now we need resilience more than performance, we compute it,
+        // but it's wasted CPU cycles ; refactor at will.
+        $total = 0;
+        foreach ($tallies as $tally) {
+            $total += $tally;
+        }
+        //////////////////////////////
+
+        $adjustedTotal = $total - 1;
+        if ( ! $low) {
+            $adjustedTotal = $total + 1;
+        }
+
+        $medianIndex = intdiv($adjustedTotal, 2);
+        $cursorIndex = 0;
+        foreach ($tallies as $gradeIndex => $tally) {
+            if (0 == $tally) {
+                continue;
+            }
+
+            $startIndex = $cursorIndex;
+            $cursorIndex += $tally;
+            $endIndex = $cursorIndex;
+
+            if (
+                $startIndex <= $medianIndex
+                &&
+                $medianIndex < $endIndex
+            ) {
+                return $gradeIndex;
+            }
+        }
+
+        return 0;
+    }
+
+
+    /**
+     * Gets details about the biggest of the two groups that did not give the median grade.
+     * This method works more generally with $aroundGradeIndex, which is set to the median grade in practice,
+     * in the current score calculus implementation above.
+     *
+     * Group Size is the amount of judgments in the group.
+     * Group Grade is the index of the grade that is adjacent to the median group.
+     * Groups may contain multiple grades, but only the closest to the median group interests us.
+     * Group Sign is:
+     * - +1 if the group promotes higher grades (adhesion)
+     * - -1 if the group promotes lower grades (contestation)
+     * - -1 by default (no judgments, or only lowest grade) ← coupled to 'LOW' MEDIAN, innit?
+     *
+     *
+     * THIS ASSUMES A "LOW" ("WORST" grade) MEDIAN IN EVEN SCENARIOS
+     *
+     *
+     * @param $aroundGradeIndex
+     * @param array $tallies
+     * @return array [groupSize, groupSign, groupGrade]
+     */
+    static function getBiggestGroup($aroundGradeIndex, array $tallies) : array
+    {
+        $belowGroupSize = 0;
+        $belowGroupSign = -1;
+        $belowGroupGrade = 0; // index
+
+        $aboveGroupSize = 0;
+        $aboveGroupSign = 1;
+        $aboveGroupGrade = 0; // index
+
+        $amountOfGrades = count($tallies);
+        for ($gradeIndex = 0; $gradeIndex < $amountOfGrades; $gradeIndex++) {
+            if (0 == $tallies[$gradeIndex]) {
+                continue;
+            }
+            if ($gradeIndex < $aroundGradeIndex) {
+                $belowGroupSize += $tallies[$gradeIndex];
+                $belowGroupGrade = $gradeIndex;
+            }
+            if ($gradeIndex > $aroundGradeIndex) {
+                $aboveGroupSize += $tallies[$gradeIndex];
+                if (0 == $aboveGroupGrade) {
+                    $aboveGroupGrade = $gradeIndex;
+                }
+            }
+        }
+
+        // /!. Assumption of LOW median with `>` /!.
+        if ($aboveGroupSize > $belowGroupSize) {
+            return [$aboveGroupSize, $aboveGroupSign, $aboveGroupGrade];
+        }
+        return [$belowGroupSize, $belowGroupSign, $belowGroupGrade];
+    }
+
+    /**
+     * Mutate the $tallies to put the judgments $fromGrade into $intoGrade.
+     * This is used by the current implementation of the score calculus.
+     *
+     * I don't like having such a method around (theoretical security concerns),
+     * so if we can rewrite the score calculus to not need such a method
+     * without introducing other mutating methods, we can and should remove this.
+     *
+     * @param $tallies
+     * @param $fromGrade
+     * @param $intoGrade
+     */
+    static function regradeJudgments(&$tallies, $fromGrade, $intoGrade)
+    {
+        $amountOfGrades = count($tallies);
+        assert(
+            $fromGrade >= 0
+            &&
+            $fromGrade < $amountOfGrades,
+            "'From' Grade is within acceptable range."
+        );
+        assert(
+            $intoGrade >= 0
+            &&
+            $intoGrade < $amountOfGrades,
+            "'Into' Grade is within acceptable range."
+        );
+
+        if ($fromGrade == $intoGrade) {
+            return;
+        }
+
+        $tallies[$intoGrade] += $tallies[$fromGrade];
+        $tallies[$fromGrade] = 0;
     }
 
 }
