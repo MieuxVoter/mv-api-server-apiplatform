4c678bd249fb457ce27902d44fb9a86435b7d452
domi41 <dominique.merle.41@gmail.com>
2021-01-10 02:32:30 +0100

    feat: connect the new MJ lib to the API
    Finally !  …  After two months and a divorce.
    We can now remove the Tallier files as well (probably).

--- src/Ranking/MajorityJudgmentRanking.php
+++ src/Ranking/MajorityJudgmentRanking.php
@@ -5,8 +5,14 @@ namespace App\Ranking;
 
 
 use App\Entity\Poll;
+use App\Entity\Poll\Proposal;
 use App\Entity\Poll\Result as PollResult;
 use App\Ranking\Options\MajorityJudgmentOptions;
+use App\Repository\PollProposalBallotRepository;
+use App\Repository\PollRepository;
+use MieuxVoter\MajorityJudgment\MajorityJudgmentDeliberator;
+use MieuxVoter\MajorityJudgment\Model\Options\MajorityJudgmentOptions as DeliberatorOptions;
+use MieuxVoter\MajorityJudgment\Model\Tally\TwoArraysPollTally;
 
 
 /**
@@ -17,6 +23,32 @@ use App\Ranking\Options\MajorityJudgmentOptions;
  */
 class MajorityJudgmentRanking implements RankingInterface
 {
+
+    /**
+     * @var PollRepository
+     */
+    protected $pollRepository;
+
+    /**
+     * @var PollProposalBallotRepository
+     */
+    protected $ballotRepository;
+
+
+    /**
+     * MajorityJudgmentRanking constructor.
+     * @param PollRepository $pollRepository
+     * @param PollProposalBallotRepository $ballotRepository
+     */
+    public function __construct(
+        PollRepository $pollRepository,
+        PollProposalBallotRepository $ballotRepository
+    )
+    {
+        $this->pollRepository = $pollRepository;
+        $this->ballotRepository = $ballotRepository;
+    }
+
 
     /**
      * The name of the Ranking, as used in the API when choosing the ranking algorithm.
@@ -44,7 +76,7 @@ class MajorityJudgmentRanking implements RankingInterface
     }
 
     /**
-     * For a given Poll; this computes a Result and returns it
+     * For a given Poll, this computes a Result and returns it
      * This is the heart of the Ranking, where the business logic resides.
      *
      * @param Poll $poll
@@ -55,9 +87,43 @@ class MajorityJudgmentRanking implements RankingInterface
     {
         $leaderboard = [];
 
+        $deliberator = new MajorityJudgmentDeliberator();
+        $options = new DeliberatorOptions();
+        $amountOfParticipants = $this->pollRepository->countParticipants($poll);
+        $tallyPerProposal = $this->ballotRepository->getTallyPerProposal($poll);
+        $pollTally = new TwoArraysPollTally($amountOfParticipants, $poll->getProposals()->toArray(), array_values($tallyPerProposal));
+        $result = $deliberator->deliberate($pollTally, $options);
+        $grades = $poll->getGradesInOrder();
 
-        // TODO: Implement resolve() method.
+//        $proposalsByUuidString = array_map(function (Proposal $p) {
+//            return $p->getUuid()->toString();
+//        }, $poll->getProposals()->toArray());
 
+        foreach ($result->getRankedProposals() as $rankedProposal) {
+
+            /** @var Proposal $proposal */
+            $proposal = $rankedProposal->getProposal();
+            $proposalResult = new Poll\Proposal\Result();
+            $proposalResult->setProposal($proposal);
+            $proposalResult->setRank($rankedProposal->getRank());
+            $proposalResult->setTally($amountOfParticipants);
+            $proposalResult->setMedianGrade($grades[$rankedProposal->getMedian()]);
+
+            $gradesResults = [];
+            $i = 0;
+            foreach ($tallyPerProposal[$proposal->getUuid()->toString()] as $gradeTally) {
+                $gradeResult = new Poll\Grade\Result();
+                $gradeResult->setProposal($proposal);
+                $gradeResult->setGrade($grades[$i]);
+                $gradeResult->setTally($gradeTally);
+
+                $gradesResults[] = $gradeResult;
+                $i++;
+            }
+            $proposalResult->setGradesResults($gradesResults);
+
+            $leaderboard[] = $proposalResult;
+        }
 
         $pollResult = new PollResult();
         $pollResult->setPoll($poll);
