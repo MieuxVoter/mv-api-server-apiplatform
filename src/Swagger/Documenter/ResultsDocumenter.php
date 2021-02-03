beb619d85964fec114ea2f2963faad4e10452d39
domi41 <dominique.merle.41@gmail.com>
2021-02-03 01:49:09 +0100

    docs: improve the documentation of the Result endpoint

---
+++ src/Swagger/Documenter/ResultsDocumenter.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace App\Swagger\Documenter;
+
+
+use App\Swagger\Documenter\Ability\TranslatorAbility;
+use App\Swagger\DocumenterInterface;
+
+
+/** @noinspection PhpUnused */
+
+
+class ResultsDocumenter implements DocumenterInterface
+{
+    use TranslatorAbility;
+
+    /**
+     * Adds custom data to the $docs and returns them.
+     *
+     * The $context helps knowing whether we're in OASv2 or OASv3.
+     *
+     * $format is "json"
+     * $context is [ "spec_version" => 2, "api_gateway" => false ]
+     *
+     * @param $docs
+     * @param $object
+     * @param string|null $format
+     * @param array $context
+     * @return array
+     */
+    public function document($docs, $object, string $format = null, array $context = []): array
+    {
+        //$version = $context['spec_version'];
+
+        //var_dump($docs['paths']['/polls/{id}/result']['get']);
+
+        $extraDocumentation = [
+            'paths' => [
+                '/polls/{id}/result' => [
+                    'get' => [
+//                        'tags' => ['Poll', 'Result'],
+                        'tags' => ['Poll'],
+
+                    // This is appended to the previous summary
+                    // We need another strategy than array_merge_recursive()
+//                        'summary' => $this->trans('result.get_for_poll.summary'),
+
+                        'description' => $this->trans('result.get_for_poll.description'),
+                    ],
+                ],
+            ],
+        ];
+
+        // Uuurgh.  That's one way to do it.  …  Let's make our own merger instead?
+        $docs['paths']['/polls/{id}/result']['get']['summary'] = $this->trans('result.get_for_poll.summary');
+
+        return array_merge_recursive($docs, $extraDocumentation);
+    }
+}
