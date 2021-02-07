a1378e62ec5574f4788cbae5b3984b962cf42032
domi41 <dominique.merle.41@gmail.com>
2021-02-07 11:02:28 +0100

    feat: update ApiPLatform from 2.4 to 2.6 and symfony to 4.4
    This yields significant changes in the API.

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
