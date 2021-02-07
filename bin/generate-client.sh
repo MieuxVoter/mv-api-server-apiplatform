7a29f90b61490e0dbed42fcca34ece1accd90b2d
domi41 <dominique.merle.41@gmail.com>
2021-02-07 21:25:32 +0100

    feat: improve the client library generation utility
    Since we moved to the latest (2.6) ApiPlatform
    our spec is not validating anymore.
    
    Let's skip the validation for today,
    but remamber to make sure the spec validates in the long run!
    To work on this, remove the --skip-validate-spec directive and generate.

--- bin/generate-client.sh
+++ bin/generate-client.sh
@@ -1,30 +1,64 @@
-#!/bin/env sh
+#!/bin/env bash
 
 TARGET="php"
+#TARGET="typescript-node"
 #TARGET="mathematica"
-NAME=MvOpenApi
+NAME=MvApi
 GIT_HOST=github.com
 USER_ID=MieuxVoter
-REPO_ID=mv-api-client-lib-${TARGET}
-OUTPUT_DIRECTORY=/tmp/${REPO_ID}
-#OUTPUT_DIRECTORY=~/code/snd/${REPO_ID}
+REPO_PREFIX=mv-api-client-lib-
+OUTPUT_DIRECTORY=/tmp
+
 VERSION=0.0.0
-SPEC_FILENAME=./openapi/spec/mvapi.${VERSION}.oas3
+SPEC_FILEPATH_NOEXT=./openapi/spec/mvapi.${VERSION}.oas3
+
+
+###
+# https://unix.stackexchange.com/questions/31414/how-can-i-pass-a-command-line-argument-into-a-shell-script/505342#505342
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -t TARGET -o OUTPUT_DIRECTORY"
+   echo -e "\t-t Target language, such as php or typoscript-node.  Defaults to php."
+   echo -e "\t-o Output directory.  Defaults to /tmp"
+   exit 1
+}
+
+while getopts "t:o:" opt
+do
+   case "$opt" in
+      t ) TARGET="$OPTARG" ;;
+      o ) OUTPUT_DIRECTORY="$OPTARG" ;;
+      ? ) helpFunction ;;
+   esac
+done
+
 
 ###
 
-bin/console api:openapi:export --spec-version=3 > ${SPEC_FILENAME}.json
-bin/console api:openapi:export --spec-version=3 --yaml > ${SPEC_FILENAME}.yaml
+REPO_ID=${REPO_PREFIX}-${TARGET}
+LIBRARY_OUTPUT_DIRECTORY=${OUTPUT_DIRECTORY}/${REPO_ID}
+TEMPLATE_DIRECTORY=./openapi/generator/${TARGET}-templates
 
 ###
+
+
+bin/console api:openapi:export --spec-version=3 > ${SPEC_FILEPATH_NOEXT}.json
+bin/console api:openapi:export --spec-version=3 --yaml > ${SPEC_FILEPATH_NOEXT}.yaml
+
+###
+
+mkdir -p ${TEMPLATE_DIRECTORY}
 
 # install: https://openapi-generator.tech/
 npx @openapitools/openapi-generator-cli generate \
+    --skip-validate-spec \
     --config ./openapi/generator/config.yml \
     --template-dir ./openapi/generator/${TARGET}-templates \
-    --input-spec ${SPEC_FILENAME}.json \
+    --input-spec ${SPEC_FILEPATH_NOEXT}.json \
     --generator-name ${TARGET} \
-    --output ${OUTPUT_DIRECTORY} \
+    --output ${LIBRARY_OUTPUT_DIRECTORY} \
     --git-host ${GIT_HOST} \
     --git-user-id ${USER_ID} \
     --git-repo-id ${REPO_ID}
