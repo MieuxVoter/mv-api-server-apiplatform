eb02f842362f0a63df56a15255d6c3de9775e877
domi41 <dominique.merle.41@gmail.com>
2021-02-10 04:02:28 +0100

    fix: hotfix the ballots subresource path

---
+++ src/Swagger/Documenter/SubresourcePathFixDocumenter.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace App\Swagger\Documenter;
+
+
+use App\Swagger\DocumenterInterface;
+use Ramsey\Uuid\Uuid;
+
+
+/** @noinspection PhpUnused */
+
+
+class SubresourcePathFixDocumenter implements DocumenterInterface {
+
+    /**
+     * Adds custom data to the $docs and returns them.
+     *
+     * The $context helps knowing whether we're in OASv2 or OASv3.
+     *
+     * $format is "json"
+     * $context is [ "spec_version" => 2, "api_gateway" => false ]
+     *
+     * @param $docs
+     * @param $object
+     * @param string|null $format
+     * @param array $context
+     * @return array
+     */
+    public function document($docs, $object, string $format = null, array $context = []): array
+    {
+        $old_path = '/polls/{id}/proposals/{proposals}/ballots';
+        $new_path = '/polls/{pollId}/proposals/{proposalId}/ballots';
+
+        $docs['paths'][$new_path]['get'] = $docs['paths'][$old_path]['get'];
+
+        unset($docs['paths'][$old_path]);
+
+        foreach ($docs['paths'][$new_path]['get']['parameters'] as $k => $parameter) {
+            if ('id' == $parameter['name']) {
+                $docs['paths'][$new_path]['get']['parameters'][$k]['name'] = 'pollId';
+                $docs['paths'][$new_path]['get']['parameters'][$k]['description'] = "Universally Unique IDentifier of the poll.";
+//                $docs['paths'][$new_path]['get']['parameters'][$k]['example'] = Uuid::uuid4();
+                $docs['paths'][$new_path]['get']['parameters'][$k]['example'] = "6c1c8973-2df3-4b5a-a17d-a3a921dba448";
+                continue;
+            }
+            if ('proposals' == $parameter['name']) {
+                $docs['paths'][$new_path]['get']['parameters'][$k]['name'] = 'proposalId';
+                $docs['paths'][$new_path]['get']['parameters'][$k]['description'] = "Universally Unique IDentifier of the proposal.";
+//                $docs['paths'][$new_path]['get']['parameters'][$k]['example'] = Uuid::uuid4();
+                $docs['paths'][$new_path]['get']['parameters'][$k]['example'] = "ebf2fda8-5f45-4a33-9758-40d7f5a74998";
+                continue;
+            }
+        }
+        
+        return $docs;
+    }
+}
