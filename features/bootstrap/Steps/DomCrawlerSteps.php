56b0affc093c20fa08cedc4c48e9a3cf9dd1f5a6
domi41 <dominique.merle.41@gmail.com>
2021-02-14 15:22:23 +0100

    feat: more handles to the chainsaw

--- features/bootstrap/Steps/DomCrawlerSteps.php
+++ features/bootstrap/Steps/DomCrawlerSteps.php
@@ -20,7 +20,7 @@ trait DomCrawlerSteps
 //    use AssertApi;
 
     /**
-     * @When /^(?:que )?(?P<actor>.+?) devr(?:ai(?:t|s|ent)|ions) obtenir un SVG validant *:?/ui
+     * @When /^(?:que )?(?P<actor>.+?) devr(?:ai(?:t|s|ent)|ions) (?:obtenir|re[çc]evoir) un SVG validant *:?/ui
      * @When /^(?P<actor>.+?) should obtain an SVG validating *:?/ui
      */
     public function actorShouldObtainSvg($actor, $pystring)
