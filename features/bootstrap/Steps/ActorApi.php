33e20684680b5b37eaa5c7565a229c03635c6f62
domi41 <dominique.merle.41@gmail.com>
2021-03-12 12:30:02 +0100

    docs: explain why the abstract traits
    I think we're going to use Traits, and see what happens.

--- features/bootstrap/Steps/ActorApi.php
+++ features/bootstrap/Steps/ActorApi.php
@@ -7,6 +7,12 @@ namespace Features\Steps;
 use App\Features\Actor;
 
 
+/**
+ * Allows code completion and early failures.
+ *
+ * Trait ActorApi
+ * @package Features\Steps
+ */
 trait ActorApi
 {
     abstract protected function actor($actorName, $createIfNone=false): Actor;
