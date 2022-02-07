e0dd0341c2cc460c7b36b6e298d8fe068fcbb426
domi41 <dominique.merle.41@gmail.com>
2022-02-07 18:39:39 +0100

    fix: string offset access syntax with curly braces is deprecated

--- src/Entity/Random/Slug.php
+++ src/Entity/Random/Slug.php
@@ -20,9 +20,16 @@ class Slug
             }
 
             for ($n = 0; $n < $length; $n++) {
-                // which is better?
-//                $slug .= substr($pool, random_int(0, $poolLength-1), 1);
-                $slug .= $pool{random_int(0, $poolLength-1)};
+                try {
+                    $dice_throw = random_int(0, $poolLength - 1);
+                } catch (\Exception $e) {
+                    trigger_error("Slug generation failed to use pseudo-random.", E_USER_ERROR);
+                    $dice_throw = 0;
+                }
+
+                $slug .= substr($pool, $dice_throw, 1);
+                // Array and string offset access syntax with curly braces is deprecated
+                #$slug .= $pool{$dice_throw};
             }
         }
 
