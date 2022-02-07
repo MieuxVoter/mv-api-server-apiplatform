32c9eb8ecdfe66b71a2c782faf742b720718a646
domi41 <dominique.merle.41@gmail.com>
2022-02-07 18:43:54 +0100

    refacto: review the Slug generator

--- src/Entity/Random/Slug.php
+++ src/Entity/Random/Slug.php
@@ -4,13 +4,18 @@
 namespace App\Entity\Random;
 
 
+use Exception;
+
+
 class Slug
 {
     static public function generate() : string
     {
+        // future possible parameters
         $lengths = [3, 2, 3];
         $separator = '-';
         $pool = "abcdefghijkmnpqrstuvwxyz";
+
         $poolLength = strlen($pool);
 
         $slug = "";
@@ -22,14 +27,12 @@ class Slug
             for ($n = 0; $n < $length; $n++) {
                 try {
                     $dice_throw = random_int(0, $poolLength - 1);
-                } catch (\Exception $e) {
+                } catch (Exception $e) {
                     trigger_error("Slug generation failed to use pseudo-random.", E_USER_ERROR);
                     $dice_throw = 0;
                 }
 
                 $slug .= substr($pool, $dice_throw, 1);
-                // Array and string offset access syntax with curly braces is deprecated
-                #$slug .= $pool{$dice_throw};
             }
         }
 
