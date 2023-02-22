00922f57ac3feb7742039552f0366fbc6d163e92
domi41 <dominique.merle.41@gmail.com>
2023-02-22 13:57:34 +0100

    features(config): bump maximum memory usage

--- features/bootstrap/bootstrap.php
+++ features/bootstrap/bootstrap.php
@@ -4,5 +4,9 @@ print("Validating the Algorithmic Constitution of a Liquid Majority Judgment App
 
 require 'functions.php';
 
+// Careful, here.  Perhaps we're leaking?
+// See https://jolicode.com/blog/you-may-have-memory-leaking-from-php-7-and-symfony-tests
+ini_set('memory_limit', '1G');
+
 putenv('APP_ENV='.$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = 'test');
 require dirname(__DIR__, 2).'/config/bootstrap.php';
